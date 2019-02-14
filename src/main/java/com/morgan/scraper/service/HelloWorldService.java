f340cf430c3f5621e5fd5ab8cfa349187f9e6a95
Thomas Morgan <tamorgan@outlook.com>
2019-02-14 17:35:45 +0000

    Added a linter and spotbugs to build
    * linter and spotbugs now checked during CI
    * .m2 directory is now cached between travisCI builds

--- src/main/java/com/morgan/scraper/service/HelloWorldService.java
+++ src/main/java/com/morgan/scraper/service/HelloWorldService.java
@@ -1,19 +1,18 @@
 package com.morgan.scraper.service;
 
+import java.time.Duration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.time.Duration;
-
 @Component
 public class HelloWorldService {
-    @Value("${name:World}")
-    private String name;
+  @Value("${name:World}")
+  private String name;
 
-    @Value("${duration:10s}")
-    private Duration duration;
+  @Value("${duration:10s}")
+  private Duration duration;
 
-    public String getHelloMessage() {
-        return "Hello " + this.name + " for " + this.duration.getSeconds() + " seconds";
-    }
+  public String getHelloMessage() {
+    return "Hello " + this.name + " for " + this.duration.getSeconds() + " seconds";
+  }
 }
